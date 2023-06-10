4d8cad5974a2d93d0de28c74b8d8b91853e6d686
iairkap <iairkap@gmail.com>
2023-06-10 15:44:40 -0300

    feature/ Container de los services

--- src/components/PricingTable/PricingTable.jsx
+++ src/components/PricingTable/PricingTable.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import PricingPlan from "../pricingPlan/pricingPLan";
+import PricingPlan from "../pricingPlan/pricingPlan";
 import styles from "./pricingTable.module.css";
 
-const PricingTable = ({ plan }) => {
+const pricingTable = ({ plan }) => {
   return (
     <div className={styles.Container}>
       {plan.map((plan, index) => (
-        <PricingPlan key={index} plan={plan} />
+        <PricingPlan
+          key={index}
+          plan={plan}
+          borderColor={index === 1 ? "#7155D9" : "#000000"}
+        />
       ))}
     </div>
   );
 };
 
-export default PricingTable;
+export default pricingTable;
